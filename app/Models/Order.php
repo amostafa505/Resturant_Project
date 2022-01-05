dd2a29f053c4f764ebd4d6d41bbb99e2599d5c82
amostafa <amostafa806@gmail.com>
2022-01-05 23:40:10 +0200

    last

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\models\User;
+use App\Models\User;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- app/Models/productImage.php
+++ app/Models/productImage.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\models\product;
+use App\Models\Product;
 
 class productImage extends Model
 {
