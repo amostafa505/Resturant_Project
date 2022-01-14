3632d2786127af73d2577166fb9fb36172f71ef1
amostafa <amostafa806@gmail.com>
2022-01-14 17:10:24 +0200

    Aws

--- config/flysystem.php
+++ config/flysystem.php
@@ -7,6 +7,7 @@ $client = new S3Client([
     'credentials' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        
     ],
     'region' => 'us-east-1',
     'version' => 'latest',
