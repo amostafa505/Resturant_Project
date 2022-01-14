e8120faab0cb57de65444b9d1ee6e6e209d5453b
amostafa <amostafa806@gmail.com>
2022-01-14 17:17:42 +0200

    Fly

---
+++ config/flysystem.php
@@ -0,0 +1,19 @@
+<?php
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
+$client = new S3Client([
+    'credentials' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    ],
+    'region' => 'us-east-1',
+    'version' => 'latest',
+]);
+
+$adapter = new AwsS3Adapter($client, 'restaurant-project',false);
+
+$filesystem = new Filesystem($adapter);
+
+?>
